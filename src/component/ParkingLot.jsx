cd5a975191dcca0bfd369d21ac5c70af43f31598
HUANGHO2 <HUANGHO2@OOCL.COM>
2024-12-06 21:16:02 +0800

    feat:generate css file to beautify the component

--- src/component/ParkingLot.jsx
+++ src/component/ParkingLot.jsx
@@ -7,32 +7,31 @@ const ParkingLot = () => {
   return (
     <div>
       {Object.keys(parkingLots).map((lot) => (
-        <div key={lot}>
-          <h3>{lot}</h3>
-          <table>
-            <thead>
-            <tr>
-              <th>Position</th>
-              <th>Available Position Number {parkingLots[lot].filter(position => !position).length}</th>
-            </tr>
-            </thead>
-            <tbody>
-            {Array.from({length: Math.ceil(parkingLots[lot].length / 3) }).map((_, rowIndex) => (
-                <tr key={rowIndex}>
-                  {Array.from({ length: 3 }).map((_, colIndex) => {
-                    const index = rowIndex * 3 + colIndex;
-                    return (
-                      <td key={index}>
-                        <div>Position {index + 1}</div>
-                        <div>{parkingLots[lot][index] || 'Empty'}</div>
-                      </td>
-                    );
-                  })}
-                </tr>
+          <div key={lot}>
+            <h3>{lot}</h3>
+            <span>Available Position Number {parkingLots[lot].filter(position => !position).length}</span>
+            <table>
+              <thead>
+              <tr>
+
+              </tr>
+              </thead>
+              <tbody>
+              {Array.from({length: Math.ceil(parkingLots[lot].length / 3)}).map((_, rowIndex) => (
+                  <tr key={rowIndex}>
+                    {Array.from({length: 3}).map((_, colIndex) => {
+                      const index = rowIndex * 3 + colIndex;
+                      return (
+                          <td key={index}>
+                            <div>{parkingLots[lot][index] || 'Empty'}</div>
+                          </td>
+                      );
+                    })}
+                  </tr>
               ))}
-            </tbody>
-          </table>
-        </div>
+              </tbody>
+            </table>
+          </div>
       ))}
     </div>
   );
